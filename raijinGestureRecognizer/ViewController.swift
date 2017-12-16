9296605b8182bd9af1a2037fac0fb30d294077a6
hidekazuuenishi <h.uenishi0804@gmail.com>
2017-12-16 14:40:55 +0900

    gesturerecognizerを練習するために作った。何もないはじめ

---
+++ raijinGestureRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  raijinGestureRecognizer
+//
+//  Created by hidekazu uenishi on 2017/12/16.
+//  Copyright © 2017年 hidekazu uenishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
